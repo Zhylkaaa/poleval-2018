201aa7cf377f4fd882d7d97c63ca29fffb959f61
Dima <zhylko.dima@gmail.com>
2021-07-24 02:04:49 +0200

    add embedding storage argument

--- scripts/train_tagger.py
+++ scripts/train_tagger.py
@@ -16,27 +16,35 @@ from models import FORWARD_LM, BACKWARD_LM, GLOVE
 from embeddings import KeyedWordEmbeddings
 from ne_groups import GROUPS
 from corpora import read_group
+from argparse import ArgumentParser
 
-embedding_types: List[TokenEmbeddings] = [
-    KeyedWordEmbeddings(GLOVE),
-    FlairEmbeddings(FORWARD_LM), #CharLMEmbeddings(FORWARD_LM),
-    FlairEmbeddings(BACKWARD_LM) #CharLMEmbeddings(BACKWARD_LM)
-]
 
-embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--embedding_storage', default='none')
+    args = parser.parse_args()
 
-for entities in GROUPS:
-    corpus: Corpus = read_group(entities)
-    tag_dictionary = corpus.make_tag_dictionary(tag_type='ner')
-    tagger: SequenceTagger = SequenceTagger(hidden_size=512,
-                                            embeddings=embeddings,
-                                            tag_dictionary=tag_dictionary,
-                                            tag_type='ner',
-                                            use_crf=True)
-    trainer: ModelTrainer = ModelTrainer(tagger, corpus)
-    file_name = '-'.join(entities)
-    trainer.train(f'data/models/{file_name}',
-                   learning_rate=0.05,
-                   mini_batch_size=124,
-                   monitor_test=True,
-                   max_epochs=40)
+    embedding_types: List[TokenEmbeddings] = [
+        KeyedWordEmbeddings(GLOVE),
+        FlairEmbeddings(FORWARD_LM), #CharLMEmbeddings(FORWARD_LM),
+        FlairEmbeddings(BACKWARD_LM) #CharLMEmbeddings(BACKWARD_LM)
+    ]
+
+    embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+    for entities in GROUPS:
+        corpus: Corpus = read_group(entities)
+        tag_dictionary = corpus.make_tag_dictionary(tag_type='ner')
+        tagger: SequenceTagger = SequenceTagger(hidden_size=512,
+                                                embeddings=embeddings,
+                                                tag_dictionary=tag_dictionary,
+                                                tag_type='ner',
+                                                use_crf=True)
+        trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+        file_name = '-'.join(entities)
+        trainer.train(f'data/models/{file_name}',
+                       learning_rate=0.05,
+                       mini_batch_size=124,
+                       monitor_test=True,
+                       embeddings_storage_mode=args.embedding_storage,
+                       max_epochs=40)
