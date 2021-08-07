aac361b22bd66874cf31beb9ab50849b6094600e
Dima <zhylko.dima@gmail.com>
2021-08-07 04:47:12 +0200

    fix file processing

--- scripts/tag.py
+++ scripts/tag.py
@@ -17,26 +17,26 @@ def pop_results(s):
 	res = list()
 	for i, t in enumerate(s):
 		g = t.get_tag('ner')
-		tp = g.split('-')[-1]
-		if (tp != opened or 'B-' in g) and opened != '':
+		tp = g.value.split('-')[-1]
+		if (tp != opened or 'B-' in g.value) and opened != '':
 			tmp = list()
 			for j in range(openedAt, i):
 				tmp.append(j+1)
 			res.append(opened + ':' + ','.join([str(x) for x in tmp]))
 			opened = ''
 			openedAt = -1
-		if g not in ('', 'O') and 'B-' in g:
+		if g.value not in ('', 'O') and 'B-' in g.value:
 			opened = tp
 			openedAt = i
 		t.add_tag('ner', '')
 	return res
 
 def tag_file(input_name='data/test.tsv',
-	         output_name='data/out.tsv',
-	         models_pattern='data/models/*/best-model.pt'):
+			 output_name='data/out.tsv',
+			 models_pattern='data/models/*/best-model.pt'):
 	taggers = list()
 	for file in glob.glob(models_pattern):
-		taggers.append(SequenceTagger.load_from_file(file))
+		taggers.append(SequenceTagger.load(file))
 	with open(input_name) as input, open(output_name, 'w') as output:
 		for line in input:
 			s = Sentence(line.rstrip())
@@ -47,4 +47,4 @@ def tag_file(input_name='data/test.tsv',
 			output.write(' '.join(res) + '\n')
 
 if __name__ == "__main__":
-    fire.Fire(tag_file)
+	fire.Fire(tag_file)
